741176d010b8fab034d44e48b462cd5fafe96d15
Paulo Henrique <97169087+PHDevss@users.noreply.github.com>
2022-11-15 23:55:38 -0300

    Finalizando

--- frontend/scripts/trailsdev.js
+++ frontend/scripts/trailsdev.js
@@ -1,7 +1,10 @@
 let trilha = JSON.parse(sessionStorage.getItem('selectedtrail_description_orange'))
 let trilhas = JSON.parse(sessionStorage.getItem('trails_orange'))
-
+let contentAtual = 0
 jQuery(function() {
+    if(!user.admin){
+        $('#addContent').hide()
+    }
     $('#trilhatitle').html(`Trilha ${trilha.name}`)
     $('#trilhadescription').html(`Trilha de conhecimento sobre ${trilha.name}`)
     attProgressBar(trilha.Progress)
@@ -52,16 +55,21 @@ function carregaSide(contents) {
     const htmlSide = contents.sort(ordenaOBJTOS).map((content)=> {
         return (
             `
-                <li class="list-group-item sidebar-menu-content d-flex justify-content-start">
+                <li class="list-group-item sidebar-menu-content d-flex justify-content-between px-2">
+                    ${!user.admin ?
+                    `<div class="form-check-label div-menu-content">
+                        <input onclick="contentView(${content.id})" id="content-${content.id}" ${content.concluded? 'checked':''} class="form-check-input input-menu" type="checkbox" value="" value="${content.id}"" />
+                    </div>`:
+                    ''}
+                    <button onclick="carregaContent(${content.id})" style="border: none; background: transparent" class="form-check-label btn-topic">${content.title}</button>
                     ${user.admin ?
-                        '':
-                        `
-                        <div class="form-check-label div-menu-content">
-                            <input id="content-${content.id}" ${content.concluded? 'checked':''} class="form-check-input input-menu" type="checkbox" onclick="contentView(${content.id})" value="${content.id}" id="${content.id}" />
-                        </div>
-                        `
-                    }
-                    <button onclick="carregaContent(${content.id})" class="form-check-label btn-topic">${content.title}</button>
+                        `<button class="px-3" style="border:none; background:transparent" onclick="deletedContent(${content.id})"><svg width="20" height="23" viewBox="0 0 20 23" fill="none" xmlns="http://www.w3.org/2000/svg" class="trash-icon">
+                            <path d="M6.25 8.25C6.66421 8.25 7 8.58579 7 9V18C7 18.4142 6.66421 18.75 6.25 18.75C5.83579 18.75 5.5 18.4142 5.5 18V9C5.5 8.58579 5.83579 8.25 6.25 8.25Z" fill="#212529"/>
+                            <path d="M10 8.25C10.4142 8.25 10.75 8.58579 10.75 9V18C10.75 18.4142 10.4142 18.75 10 18.75C9.58579 18.75 9.25 18.4142 9.25 18V9C9.25 8.58579 9.58579 8.25 10 8.25Z" fill="#212529"/>
+                            <path d="M14.5 9C14.5 8.58579 14.1642 8.25 13.75 8.25C13.3358 8.25 13 8.58579 13 9V18C13 18.4142 13.3358 18.75 13.75 18.75C14.1642 18.75 14.5 18.4142 14.5 18V9Z" fill="#212529"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.75 4.5C19.75 5.32843 19.0784 6 18.25 6H17.5V19.5C17.5 21.1569 16.1569 22.5 14.5 22.5H5.5C3.84315 22.5 2.5 21.1569 2.5 19.5V6H1.75C0.921573 6 0.25 5.32843 0.25 4.5V3C0.25 2.17157 0.921573 1.5 1.75 1.5H7C7 0.671573 7.67157 0 8.5 0H11.5C12.3284 0 13 0.671573 13 1.5H18.25C19.0784 1.5 19.75 2.17157 19.75 3V4.5ZM4.17705 6L4 6.08853V19.5C4 20.3284 4.67157 21 5.5 21H14.5C15.3284 21 16 20.3284 16 19.5V6.08853L15.8229 6H4.17705ZM1.75 4.5V3H18.25V4.5H1.75Z" fill="#212529"/>
+                        </svg></button>`:
+                    ''}
                 </li>
             `
         )
@@ -69,18 +77,18 @@ function carregaSide(contents) {
     htmlSide.unshift(`
     <li class="nav-item w-100">
         <div class="title-track d-flex flex-row justify-content-start align-item-center">
-            <h4>1. O início</h4>
+            <h4>1. O início</h4>       
         </div>
     </li>`)
     htmlSide.push(`
     <li>
         <div class="title-track d-flex flex-row justify-content-start align-item-center">
-            <h4>2. Conceitos básicos</h4>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-                <path
-                d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"
+        <h4>2. Conceitos básicos</h4>
+        <svg xmlns="../assets/cadeado.png" width="20" height="22" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+            <path
+            d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"
             />
-            </svg>
+        </svg>
         </div>
     </li>
     `)
@@ -99,7 +107,7 @@ function carregaContent(id) {
             reference = `<a href="${content.reference}" target="_blank">${content.reference}</a>`
         }else{
             reference = `
-            <iframe width="100%" height="500" src="https://www.youtube.com/embed/${content.reference.slice(32)}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="800" src="https://www.youtube.com/embed/${content.reference.slice(32)}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             `
         }
     } else {
@@ -107,6 +115,8 @@ function carregaContent(id) {
     }
 
     $('#bodycontent').html(reference)
+    contentAtual = id
+
 }
 
 function contentView(id) {
@@ -162,8 +172,6 @@ function contentView(id) {
             // // sessionStorage.setItem('trails_orange', JSON.stringify())
             // // trails_orange
             // // console.log(contentsOBJ.contents.find(content => content.id === id))
-            console.log('Trilhas =>', trilhas)
-            console.log('TrilhaAtualizada =>', trilha)
             const newTrilhas = trilhas.map(content => {
                 if(content.id === idSelectionTrail){
                     return trilha
@@ -202,6 +210,40 @@ function attProgressBar(progress) {
         
         $('#progressbar').html(htmlProgress)
     }
+}
+
+function deletedContent(id) {
+    settingsDeleted = {
+        "url": `http://localhost:3000/content/${id}`,
+        "method": "DELETE",
+        "timeout": 0,
+        "headers": {
+            "Authorization": token
+        },
+        "success":function (data) {
+            carregaContentTrilhaAPI()
+        },
+        "error": function (err) {
+            
+        }
+    };
+      
+      $.ajax(settingsDeleted)
+}
+
+function nextContent() {
+    let novoContent
+    if(!contentAtual){
+        novoContent = 1
+    } else {
+        novoContent = contentAtual+1;
+    }
+    if(novoContent > trilha.Total_contents){
+        window.location.assign("../trilhas/trilhas.html");
+        return
+    }
+    carregaContent(contentAtual+1)
+    contentView(novoContent)
 }
 
 
